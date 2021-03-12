46044ea1fd0fbac69ac0e8abfefaa5209dcd93d6
Andy Kim <andy.kim@optus.com.au>
2021-03-12 22:32:52 +1100

    Initial Commit

---
+++ SwiftUIExtension/SwiftUIExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExtensionApp.swift
+//  SwiftUIExtension
+//
+//  Created by Andy Kim on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
